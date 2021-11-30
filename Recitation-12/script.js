0249cc3403c6bc3c8df6b7b95106e902611f5dfd
Amalaga19 <arm912@nyu.edu>
2021-12-01 02:38:15 +0800

    Corrected Recitation 12

--- Recitation-12/script.js
+++ Recitation-12/script.js
@@ -1,205 +1,194 @@
 let img;
 let cam;
-let img1;
-let img2;
-let img3;
-
+let mode;
+let diameter;
+let shape;
+let shapeNum;
 function setup() {
-  let canvas= createCanvas(1280, 480);
+  let canvas= createCanvas(640, 480);
   canvas.parent(canvasContainer);
   cam = createCapture(VIDEO);
-  cam.size(320, 240);
   cam.hide();
-  img = createImage(width/4, height/2);
-  img2 = createImage(width/4, height/2);
-  img3 = createImage(width/4, height/2);
-  img4 = createImage(width/4, height/2);
-  img5 = createImage(width/4, height/2);
-  img6 = createImage(width/4, height/2);
-  img7 = createImage(width/4, height/2);
-  img8 = createImage(width/4, height/2);
+  img = createImage(width, height);
+  mode = int(random(1,9));
+  shape = random();
   frameRate(60)
+  shapeNum = int(random(2, 15));
+  noStroke();
 }
 
 function draw() {
+  let pixelWidth = document.getElementById("pixelWidth");
+  let pixelWidth1 = pixelWidth.value;
+  let pixelHeight = document.getElementById("pixelHeight");
+  let pixelHeight1 = pixelHeight.value;
   background(0);
-  tintRedBlue(img, 0, 0);
-  tintBlueRed(img2, 0,height/2);
-  tintYellowBlue(img3, width/4, height/2);
-  tintBlueYellow(img4, width/4, 0);
-  tintPinkBlue(img5, 2*width/4, 0);
-  tintBluePink(img6, 2*width/4, height/2);
-  tintGreenBlue(img7, 3*width/4, 0);
-  tintBlueGreen(img8, 3*width/4, height/2);
+  if (mode == 1){tintRedBlue(img,pixelWidth1, pixelHeight1, shapeNum);}
+  else if (mode == 2){tintBlueRed(img, pixelWidth1, pixelHeight1, shapeNum);}
+  else if (mode == 3){tintYellowBlue(img, pixelWidth1, pixelHeight1, shapeNum);}
+  else if (mode == 4){tintBlueYellow(img, pixelWidth1, pixelHeight1, shapeNum);}
+  else if (mode == 5){tintPinkBlue(img, pixelWidth1, pixelHeight1, shapeNum);}
+  else if (mode == 6){tintBluePink(img, pixelWidth1, pixelHeight1, shapeNum);}
+  else if (mode == 7){tintGreenBlue(img, pixelWidth1, pixelHeight1, shapeNum);}
+  else if (mode == 8){tintBlueGreen(img, pixelWidth1, pixelHeight1, shapeNum);}
 }
 
-function tintRedBlue(imageName, coordX, coordY){
+function tintRedBlue(imageName, shapeWidth, shapeHeight, shapeNumber){
+cam.loadPixels();
+imageName.loadPixels();
+for (let y = 0; y < imageName.height; y+= shapeNumber) {
+  for (let x = 0; x < imageName.width; x+= shapeNumber) {
+    let index = (x + y * imageName.width) * 4;
+    let r = cam.pixels[index + 0];
+    let g = cam.pixels[index + 1];
+    let b = cam.pixels[index + 2];
+
+    fill(r, 255-g, 255-b);
+    if (shape>0 && shape<0.5){
+      rect(x,y, shapeWidth, shapeHeight);
+    }
+    else if (shape>=0.5 && shape<=1){
+      ellipse(x,y, shapeWidth, shapeHeight);
+    }
+  }
+}
+}
+function tintBlueRed(imageName, shapeWidth, shapeHeight, shapeNumber){
   cam.loadPixels();
   imageName.loadPixels();
-  for (let y = 0; y < imageName.height; y++) {
-    for (let x = 0; x < imageName.width; x++) {
+  for (let y = 0; y < imageName.height; y+= shapeNumber) {
+    for (let x = 0; x < imageName.width; x+= shapeNumber) {
       let index = (x + y * imageName.width) * 4;
-
       let r = cam.pixels[index + 0];
       let g = cam.pixels[index + 1];
       let b = cam.pixels[index + 2];
 
-      imageName.pixels[index + 0] = r;
-      imageName.pixels[index + 1] = 255-g;
-      imageName.pixels[index + 2] = 255-b;
-      imageName.pixels[index + 3] = 255;
+      fill(255-r, g, b);
+      if (shape>0 && shape<0.5){
+        rect(x,y, shapeWidth, shapeHeight);
+      }
+      else if (shape>=0.5 && shape<=1){
+        ellipse(x,y,shapeWidth, shapeHeight);
+      }
     }
-
   }
-   imageName.updatePixels();
-   image(imageName, coordX, coordY);
-
 }
-function tintBlueRed(imageName, coordX, coordY){
+function tintYellowBlue(imageName, shapeWidth, shapeHeight, shapeNumber){
+    cam.loadPixels();
+    imageName.loadPixels();
+    for (let y = 0; y < imageName.height; y+= shapeNumber) {
+      for (let x = 0; x < imageName.width; x+= shapeNumber) {
+      let index = (x + y * imageName.width) * 4;
+      let r = cam.pixels[index + 0];
+      let g = cam.pixels[index + 1];
+      let b = cam.pixels[index + 2];
+      fill(r, g, 255-b);
+      if (shape>0 && shape<0.5){
+        rect(x,y, shapeWidth, shapeHeight);
+      }
+      else if (shape>=0.5 && shape<=1){
+        ellipse(x,y,shapeWidth, shapeHeight);
+      }
+    }
+  }
+  }
+function tintBlueYellow(imageName, shapeWidth, shapeHeight, shapeNumber){
   cam.loadPixels();
   imageName.loadPixels();
-
-  for (let y = 0; y < imageName.height; y++) {
-    for (let x = 0; x < imageName.width; x++) {
+  for (let y = 0; y < imageName.height; y+= shapeNumber) {
+    for (let x = 0; x < imageName.width; x+= shapeNumber) {
       let index = (x + y * imageName.width) * 4;
 
       let r = cam.pixels[index + 0];
       let g = cam.pixels[index + 1];
       let b = cam.pixels[index + 2];
-
-      imageName.pixels[index + 0] = 255-r;
-      imageName.pixels[index + 1] = g;
-      imageName.pixels[index + 2] = b;
-      imageName.pixels[index + 3] = 255;
+      fill(255-r, 255-g, b);
+      if (shape>0 && shape<0.5){
+        rect(x,y, shapeWidth, shapeHeight);
+      }
+      else if (shape>=0.5 && shape<=1){
+        ellipse(x,y,shapeWidth, shapeHeight);
+      }
     }
   }
-  imageName.updatePixels();
-  image(imageName, coordX, coordY);
 }
-function tintYellowBlue(imageName, coordX, coordY){
+function tintBluePink(imageName, shapeWidth, shapeHeight, shapeNumber){
+cam.loadPixels();
+imageName.loadPixels();
+for (let y = 0; y < imageName.height; y+= shapeNumber) {
+  for (let x = 0; x < imageName.width; x+= shapeNumber) {
+    let index = (x + y * imageName.width) * 4;
+    let r = cam.pixels[index + 0];
+    let g = cam.pixels[index + 1];
+    let b = cam.pixels[index + 2];
+
+    fill(r, 0, 255);
+    if (shape>0 && shape<0.5){
+      rect(x,y, shapeWidth, shapeHeight);
+    }
+    else if (shape>=0.5 && shape<=1){
+      ellipse(x,y,shapeWidth, shapeHeight);
+    }
+  }
+}
+}
+function tintPinkBlue(imageName, shapeWidth, shapeHeight, shapeNumber){
+cam.loadPixels();
+imageName.loadPixels();
+for (let y = 0; y < imageName.height; y+= shapeNumber) {
+  for (let x = 0; x < imageName.width; x+= shapeNumber) {
+    let index = (x + y * imageName.width) * 4;
+    let r = cam.pixels[index + 0];
+    let g = cam.pixels[index + 1];
+    let b = cam.pixels[index + 2];
+
+    fill(255-r, g, 255);
+    if (shape>0 && shape<0.5){
+      rect(x,y, shapeWidth, shapeHeight);
+    }
+    else if (shape>=0.5 && shape<=1){
+      ellipse(x,y,shapeWidth, shapeHeight);
+    }
+  }
+}
+}
+function tintGreenBlue(imageName, shapeWidth, shapeHeight, shapeNumber){
   cam.loadPixels();
   imageName.loadPixels();
-
-  for (let y = 0; y < imageName.height; y++) {
-    for (let x = 0; x < imageName.width; x++) {
+  for (let y = 0; y < imageName.height; y+= shapeNumber) {
+    for (let x = 0; x < imageName.width; x+= shapeNumber) {
       let index = (x + y * imageName.width) * 4;
-
       let r = cam.pixels[index + 0];
       let g = cam.pixels[index + 1];
       let b = cam.pixels[index + 2];
 
-      imageName.pixels[index + 0] = r;
-      imageName.pixels[index + 1] = g;
-      imageName.pixels[index + 2] = 255-b;
-      imageName.pixels[index + 3] = 255;
+      fill(0, 255-g, b);
+      if (shape>0 && shape<0.5){
+        rect(x,y, shapeWidth, shapeHeight);
+      }
+      else if (shape>=0.5 && shape<=1){
+        ellipse(x,y,shapeWidth, shapeHeight);
+      }
     }
   }
-
-  imageName.updatePixels();
-  image(imageName, coordX, coordY);
-
 }
-function tintBlueYellow(imageName, coordX, coordY){
+function tintBlueGreen(imageName, shapeWidth, shapeHeight, shapeNumber){
   cam.loadPixels();
   imageName.loadPixels();
-
-  for (let y = 0; y < imageName.height; y++) {
-    for (let x = 0; x < imageName.width; x++) {
+  for (let y = 0; y < imageName.height; y+= shapeNumber) {
+    for (let x = 0; x < imageName.width; x+= shapeNumber) {
       let index = (x + y * imageName.width) * 4;
-
       let r = cam.pixels[index + 0];
       let g = cam.pixels[index + 1];
       let b = cam.pixels[index + 2];
 
-      imageName.pixels[index + 0] = 255-r;
-      imageName.pixels[index + 1] = 255-g;
-      imageName.pixels[index + 2] = b;
-      imageName.pixels[index + 3] = 255;
+      fill(0, g, 255-b);
+      if (shape>0 && shape<0.5){
+        rect(x,y, shapeWidth, shapeHeight);
+      }
+      else if (shape>=0.5 && shape<=1){
+        ellipse(x,y,shapeWidth, shapeHeight);
+      }
     }
   }
-  imageName.updatePixels();
-  image(imageName, coordX, coordY);
-
-}
-function tintBluePink(imageName, coordX, coordY){
-  cam.loadPixels();
-  imageName.loadPixels();
-
-  for (let y = 0; y < imageName.height; y++) {
-    for (let x = 0; x < imageName.width; x++) {
-      let index = (x + y * imageName.width) * 4;
-
-      let r = cam.pixels[index + 0];
-      let g = cam.pixels[index + 1];
-      let b = cam.pixels[index + 2];
-
-      imageName.pixels[index + 0] = r;
-      imageName.pixels[index + 1] = 0;
-      imageName.pixels[index + 2] = 255;
-      imageName.pixels[index + 3] = 255;
-    }
   }
-  imageName.updatePixels();
-  image(imageName, coordX, coordY);
-}
-function tintPinkBlue(imageName, coordX, coordY){
-  cam.loadPixels();
-  imageName.loadPixels();
-
-  for (let y = 0; y < imageName.height; y++) {
-    for (let x = 0; x < imageName.width; x++) {
-      let index = (x + y * imageName.width) * 4;
-
-      let r = cam.pixels[index + 0];
-      let g = cam.pixels[index + 1];
-      let b = cam.pixels[index + 2];
-
-      imageName.pixels[index + 0] = 255-r;
-      imageName.pixels[index + 1] = g;
-      imageName.pixels[index + 2] = 255;
-      imageName.pixels[index + 3] = 255;
-    }
-  }
-  imageName.updatePixels();
-  image(imageName, coordX, coordY);
-}
-function tintGreenBlue(imageName, coordX, coordY){  cam.loadPixels();
-  imageName.loadPixels();
-
-  for (let y = 0; y < imageName.height; y++) {
-    for (let x = 0; x < imageName.width; x++) {
-      let index = (x + y * imageName.width) * 4;
-
-      let r = cam.pixels[index + 0];
-      let g = cam.pixels[index + 1];
-      let b = cam.pixels[index + 2];
-
-      imageName.pixels[index + 0] = 0;
-      imageName.pixels[index + 1] = 255-g;
-      imageName.pixels[index + 2] = b;
-      imageName.pixels[index + 3] = 255;
-    }
-  }
-  imageName.updatePixels();
-  image(imageName, coordX, coordY);
-}
-function tintBlueGreen(imageName, coordX, coordY){  cam.loadPixels();
-  imageName.loadPixels();
-
-  for (let y = 0; y < imageName.height; y++) {
-    for (let x = 0; x < imageName.width; x++) {
-      let index = (x + y * imageName.width) * 4;
-
-      let r = cam.pixels[index + 0];
-      let g = cam.pixels[index + 1];
-      let b = cam.pixels[index + 2];
-
-      imageName.pixels[index + 0] = 0;
-      imageName.pixels[index + 1] = g;
-      imageName.pixels[index + 2] = 255-b;
-      imageName.pixels[index + 3] = 255;
-    }
-  }
-  imageName.updatePixels();
-  image(imageName, coordX, coordY);
-}
